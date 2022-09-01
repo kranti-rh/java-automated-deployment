2980edaeadfa82db8844e8964726737485fce03a
Kranti Prasad <krprasad@redhat.com>
2022-09-01 19:25:47 +0530

    update providers

--- providers.tf
+++ providers.tf
@@ -2,10 +2,3 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
-
-terraform {
-  backend "gcs" {
-    bucket = "myvik-tf-state-prod"
-    prefix = "terraform/state"
-  }
-}
