2ac9b28e0ed711ba9abdfb89ac8f369c881bd188
Kranti Prasad <krprasad@redhat.com>
2022-09-01 17:52:01 +0530

    format main

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "google_storage_bucket" "bucket" {
   name = "test-bucket-random-001122"
-  location      = "EU"
+  location = "EU"
 }
 
 resource "google_storage_bucket" "gcs_bucket" {
   name = "test-bucket-random-001123"
-  location      = "EU"
+  location = "EU"
 }
