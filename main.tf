85a5df317a43be1b2633688a32a1377bc528865b
Kranti Prasad <krprasad@redhat.com>
2022-09-01 19:31:33 +0530

    update main and providers

--- main.tf
+++ main.tf
@@ -2,8 +2,3 @@ resource "google_storage_bucket" "bucket" {
   name = "test-bucket-random-001122"
   location = "EU"
 }
-
-resource "google_storage_bucket" "gcs_bucket" {
-  name = "test-bucket-random-001123"
-  location = "EU"
-}
--- providers.tf
+++ providers.tf
@@ -2,3 +2,10 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+
+terraform {
+  backend "gcs" {
+    bucket = "myvik-tf-state-prod"
+    prefix = "terraform/state"
+  }
+}
